3a091c174a299fb5568bb518d384be844d06046f
LinuxOpSys <120705038+linuxopsys@users.noreply.github.com>
2022-12-19 03:51:48 +0000

    Add files via upload

---
+++ Kali-Dark.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# Kali-Dark 
+# --- ----
+# Gnome Terminal color scheme install script
+# auther - LinuxOpSys
+# Website - linuxopsys.com
+# Twitter - twitter.com/linuxopsys
+
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Kali-Dark"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="Kali-Dark"
+[[ -z "$DCONF" ]] && DCONF=dconf
+[[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
+
+dset() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    if [[ "$type" == "string" ]]; then
+        val="'$val'"
+    fi
+
+    "$DCONF" write "$PROFILE_KEY/$key" "$val"
+}
+
+# because dconf still doesn't have "append"
+dlist_append() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$DCONF" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "'$val'"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$DCONF" write "$key" "[$entries]"
+}
+
+# Newest versions of gnome-terminal use dconf
+if which "$DCONF" > /dev/null 2>&1; then
+    [[ -z "$BASE_KEY_NEW" ]] && BASE_KEY_NEW=/org/gnome/terminal/legacy/profiles:
+
+    if [[ -n "`$DCONF list $BASE_KEY_NEW/`" ]]; then
+        if which "$UUIDGEN" > /dev/null 2>&1; then
+            PROFILE_SLUG=`uuidgen`
+        fi
+
+        if [[ -n "`$DCONF read $BASE_KEY_NEW/default`" ]]; then
+            DEFAULT_SLUG=`$DCONF read $BASE_KEY_NEW/default | tr -d \'`
+        else
+            DEFAULT_SLUG=`$DCONF list $BASE_KEY_NEW/ | grep '^:' | head -n1 | tr -d :/`
+        fi
+
+        DEFAULT_KEY="$BASE_KEY_NEW/:$DEFAULT_SLUG"
+        PROFILE_KEY="$BASE_KEY_NEW/:$PROFILE_SLUG"
+
+        # copy existing settings from default profile
+        $DCONF dump "$DEFAULT_KEY/" | $DCONF load "$PROFILE_KEY/"
+
+        # add new copy to list of profiles
+        dlist_append $BASE_KEY_NEW/list "$PROFILE_SLUG"
+
+        # update profile values with theme options
+        dset visible-name "'$PROFILE_NAME'"
+        dset palette "['#1f2229', '#d41919', '#5ebdab', '#fea44c', '#367bf0', '#9755b3', '#49aee6', '#e6e6e6', '#198388', '#ec0101', '#47d4b9', '#ff8a18', '#277fff', '#962ac3', '#05a1f7', '#ffffff']"
+        dset background-color "'#1f2229'"
+        dset foreground-color "'#ffffff'"
+        dset bold-color "'##e6e6e6'"
+        dset bold-color-same-as-fg "true"
+        dset use-theme-colors "false"
+        dset use-theme-background "false"
+
+        unset PROFILE_NAME
+        unset PROFILE_SLUG
+        unset DCONF
+        unset UUIDGEN
+        exit 0
+    fi
+fi
+
